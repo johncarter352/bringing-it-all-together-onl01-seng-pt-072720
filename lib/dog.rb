636b5baf15279cdb3cb51651639503bd559591da
John Carter <johncarter352@gmail.com>
2020-10-30 17:23:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,8 +12,30 @@ class Dog
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs(id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
     SQL
+    DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS dogs
+    SQL
+  end
+  
+  def self.new_from_db(row)
+    
+  end
+  
+  def self.find_by_name
+    
+  end
+  
+  def update
+    
+  end
+  
+  def save
+    
+  end
   
   
   
